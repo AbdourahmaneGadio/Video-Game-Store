d99109b51fcaa5daec4e23abc0846394bcbdbc34
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2024-06-15 17:27:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 $ROOT_PATH = "http://localhost/Video-Game-Store";
 require ("authenticate.php"); 
 require_once("controllers/gamesController.php");
