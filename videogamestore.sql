22efa0728cb1d6a2776efa036e88860784839093
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2024-06-18 23:10:19 +0700

    Update videogamestore.sql

--- videogamestore.sql
+++ videogamestore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mar. 18 juin 2024 à 15:42
+-- Généré le : mar. 18 juin 2024 à 16:10
 -- Version du serveur : 10.11.6-MariaDB-0+deb12u1
 -- Version de PHP : 8.2.18
 
@@ -49,7 +49,7 @@ INSERT INTO `games` (`id`, `visual`, `resume`, `rating`, `year`, `title`, `edito
 (8, '8.', 'Dragon Ball Z: Budokai Tenkaichi 3, originally published in Japan as Dragon Ball Z: Sparking! METEOR (ドラゴンボールＺ　Ｓｐａｒｋｉｎｇ！ＭＥＴＥＯＲ Doragon Bōru Zetto　Supākingu! Meteo) in Japan, is the third installment of the Budokai Tenkaichi series. Like its predecessor, despite being released under the Dragon Ball Z label, Budokai Tenkaichi 3 essentially touches upon all series installments of the Dragon Ball franchise, featuring numerous characters and stages set in Dragon Ball, Dragon Ball Z, Dragon Ball GT and numerous film adaptations of Z.', '12', 2007, 'Dragon Ball Z: Budokai Tenkaichi 3', 'Namco Bandai', 'https://www.youtube.com/embed/sfbMHbFiN08', '2024-06-15', 0),
 (9, '9.', 'SSX 3 is a snowboarding video game developed by EA Canada and published by Electronic Arts under the EA Sports BIG label. The game was originally released on October 21, 2003, for the PlayStation 2, Xbox, and GameCube. It was later ported to the Game Boy Advance by Visual Impact on November 11, 2003, and to the Gizmondo by Exient Entertainment on August 31, 2005, as a launch title. It is the third installment in the SSX series.', '3', 2005, 'SSX 3', 'EA Sports BIG', 'https://www.youtube.com/embed/qx7MtNtPwjQ', '2024-06-15', 0),
 (11, '11.', 'SSX on Tour is a snowboarding and skiing game, the fourth title in the SSX series of video games for the GameCube, PlayStation 2, Xbox and PlayStation Portable. It was released in North America on October 11, 2005 and in the PAL region on October 21, 2005. The PlayStation Portable version was released in Europe on October 28, 2005. In 2007, a prequel titled SSX Blur was released, which took place between SSX 3 and SSX on Tour.', '3', 2005, 'SSX On Tour', 'EA Sports BIG', 'https://www.youtube.com/embed/H6EqWNpS39o', '2024-06-17', 0),
-(12, '.jpeg', 'Marvels Spider-Man 2 is a 2023 action-adventure game developed by Insomniac Games and published by Sony Interactive Entertainment. It is based on the Marvel Comics character Spider-Man, and features a narrative inspired by its long-running comic book mythology which is also derived from various adaptations in other media.', '2', 2023, 'Spider-Man 2', 'Insomniac Games', 'https://www.youtube.com/embed/9pBsUw0RI3Y', '2024-06-18', 0);
+(12, '12.', 'Marvels Spider-Man 2 is a 2023 action-adventure game developed by Insomniac Games and published by Sony Interactive Entertainment. It is based on the Marvel Comics character Spider-Man, and features a narrative inspired by its long-running comic book mythology which is also derived from various adaptations in other media.', '16', 2023, 'Spider-Man 2', 'Insomniac Games', 'https://www.youtube.com/embed/9pBsUw0RI3Y', '2024-06-18', 0);
 
 -- --------------------------------------------------------
 
