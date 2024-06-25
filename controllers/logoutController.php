c6437f86ffca29ef026cf1402344dc349b7286e9
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2024-06-25 14:48:20 +0700

    Correction cookies

--- controllers/logoutController.php
+++ controllers/logoutController.php
@@ -1,5 +1,8 @@
 <?php
 setcookie ("user_login",$_SESSION['name'],time()-42000, "/");
+setcookie ("user_statut",$_SESSION['admin'],time()-42000, "/");
+setcookie ("user_id",$_SESSION['id'],time()-42000, "/");
+
 unset($_COOKIE['user_login']);
 unset($_COOKIE['user_statut']);
 unset($_COOKIE['user_id']);
